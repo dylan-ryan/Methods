7dcd32542ec24c5ebae47ad8be2464f6c1560867
Dylan <dylanryan431@gmail.com>
2023-09-19 14:48:46 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dylan();
+
+            Console.ReadKey(true);
+        }
+
+        static void Dylan()
+        {
+            Console.WriteLine("Dylan"); 
+        }
+    }
+}
