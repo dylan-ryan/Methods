194f91e19d8894f2a773255caf1a0984eed8421d
dylan-ryan <dylanryan431@gmail.com>
2023-09-19 18:08:11 -0300

    Update Program.cs

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,15 +10,50 @@ namespace Methods
     internal class Program
     {
         static int health;
+        static int lives;
+        static int score;
         static string name;
         static void Main()
         {
             health = 100;
             name = "dylan";
+            score = 0;
+            lives = 3;
+
 
             ShowHud();
+            Console.WriteLine("Punched by Troll");
+            TakeDamage(10);
+            ShowHud();
+            Console.WriteLine("Killed Troll");
+            GainScore(10);
+            ShowHud();
+            Console.WriteLine("Fell of a cliff");
+            TakeLife(1);
+            ShowHud();
+            Console.WriteLine("Hit a Jackpot");
+            GainScore(1000000);
+            ShowHud();
+            Console.WriteLine("Violently Mugged");
+            LoseScore(1000000);
+            TakeDamage(89);
+            ShowHud();
+
 
             Console.ReadKey(true);
+        }
+
+        static void GainScore(int winner)
+        {
+            score = score + winner;
+        }
+        static void LoseScore(int trash)
+        {
+            score = score - trash;
+        }
+        static void TakeLife(int llost)
+        {
+            lives = lives - llost;
         }
         static void TakeDamage(int hp)
         {
@@ -27,8 +62,12 @@ namespace Methods
 
         static void ShowHud()
         {
+            Console.WriteLine("--------------------");
             Console.WriteLine(name);
-            Console.WriteLine(health);
+            Console.WriteLine("HP = " + health);
+            Console.WriteLine("Score = " + score);
+            Console.WriteLine("Lives = " + lives);
+            Console.WriteLine("--------------------");
         }
     }
 
